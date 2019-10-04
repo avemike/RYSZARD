56a6188ab4e56a595ebd0aa0c569260eba78fd37
dominicus28 <dominik.domin740@gmail.com>
2019-10-04 11:24:31 +0200

    Database connection

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
 // Kickstart the framework
 $f3=require('lib/base.php');
 
+// Dataase connection 
+$db=new DB\SQL(
+    'mysql:host=localhost;port=3306;dbname=ryszardDB',
+    'root'
+);
+
 $f3->route('GET /',
 	function($f3) {
 		echo \Template::instance()->render('ui/profile.html');
