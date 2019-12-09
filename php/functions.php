f58b3129b52cb011dc6ddda59d23b338150bcc5e
avemike <fajny.michal.gajek@gmail.com>
2019-12-09 18:24:27 +0100

    improved mail module: now works fine + moved html mail files to folder mail

--- php/functions.php
+++ php/functions.php
@@ -4,26 +4,28 @@
             global $db;
             $result = $db->exec('SELECT mail_date, mail_title, mail_receiver, mail_content, nickname FROM mail LEFT JOIN characters ON mail.mail_receiver=characters.char_id WHERE mail_sender=?', array($_SESSION["char_id"]));
             $f3->set('result', $result);
-            echo \Template::instance()->render('outbox.html');
+            echo \Template::instance()->render('mail/outbox.html');
         }
         function getinbox($f3){
             global $db;
             $result = $db->exec('SELECT mail_date, mail_title, mail_sender, mail_content, nickname FROM mail LEFT JOIN characters ON mail.mail_sender=characters.char_id WHERE mail_sender!=?', array($_SESSION["char_id"]));
             $f3->set('result', $result);
-            echo \Template::instance()->render('inbox.html');
+            echo \Template::instance()->render('mail/inbox.html');
         }
         function getmail($f3){
-            echo \Template::instance()->render('mail.html');    
+            echo \Template::instance()->render('mail/mail.html');    
         }
         function postmail($f3){
             global $db;
             if($result = $db->exec('SELECT char_id FROM characters WHERE nickname=? AND server_id=?', array($_POST["address"], $_SESSION["server"]))){
                 $db->exec('INSERT INTO mail (mail_receiver, mail_content, mail_title, mail_sender) values (?, ?, ?, ?)', array($result[0]["char_id"], htmlspecialchars($_POST["content"]), $_POST["title"], $_SESSION["char_id"])); 
             }
-            else{
-                $f3->set('mailerror', 'Podany użytkownik nie istnieje');
-            }
-            echo \Template::instance()->render('mail.html');
+
+            // else{
+            //     $f3->set('mailerror', 'Podany użytkownik nie istnieje');
+            // }
+            
+            echo \Template::instance()->render('mainpage.html');
         }
     }
     class home{
